f4735cc2f46fa9065f14a5167aeb4a27300724b0
nob-kat <kato@aquapharmacy.co.jp>
2023-11-14 00:59:11 +0900

    Javaプログラミング入門学習終了

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+    //mainメソッドです!
+    public static void main(String[] args) {
+        //System.out.println("TechAcademy");
+        /*System.out.println("Java");
+        System.out.println("学習を始めましょう");
+    */
+        System.out.println(number1);
+
+    }
+
+    int number1;
+    {
+    number1 = 1;
+    }
+    String str1;
+    {
+    str1 = "abc";
+    }
+}
+
+
